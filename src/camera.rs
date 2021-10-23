d14d5cf1552330439227d8f5f8bebfe5042406b4
Chris Lieb <chris.lieb@gmail.com>
2021-10-22 21:43:30 -0400

    Chapter 11: Defocus blur

--- src/camera.rs
+++ src/camera.rs
@@ -1,15 +1,30 @@
 use crate::ray::Ray;
 use crate::vec::Vec3;
 
+use rand::Rng;
+
 pub struct Camera {
     origin: Vec3,
     lower_left_corner: Vec3,
     horizontal: Vec3,
     vertical: Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3,
+    lens_radius: f32,
 }
 
 impl Camera {
-    pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3, vfov: f32, aspect: f32) -> Camera {
+    pub fn new(
+        lookfrom: Vec3,
+        lookat: Vec3,
+        vup: Vec3,
+        vfov: f32,
+        aspect: f32,
+        aperture: f32,
+        focust_dist: f32,
+    ) -> Camera {
+        let lens_radius = aperture / 2.0;
         let theta = vfov * std::f32::consts::PI / 180.0;
         let half_height = (theta / 2.0).tan();
         let half_width = aspect * half_height;
@@ -17,21 +32,39 @@ impl Camera {
         let w = (lookfrom - lookat).unit_vector();
         let u = vup.cross(&w).unit_vector();
         let v = w.cross(&u);
-        let lower_left_corner = origin - half_width * u - half_height * v - w;
-        let horizontal = 2.0 * half_width * u;
-        let vertical = 2.0 * half_height * v;
+        let lower_left_corner =
+            origin - half_width * focust_dist * u - half_height * focust_dist * v - focust_dist * w;
+        let horizontal = 2.0 * half_width * focust_dist * u;
+        let vertical = 2.0 * half_height * focust_dist * v;
         Camera {
             origin,
             lower_left_corner,
             horizontal,
             vertical,
+            u,
+            v,
+            w,
+            lens_radius,
         }
     }
 
-    pub fn get_ray(&self, u: f32, v: f32) -> Ray {
+    pub fn get_ray(&self, s: f32, t: f32) -> Ray {
+        let rd = self.lens_radius * random_in_unit_disk();
+        let offset = self.u * rd.x + self.v * rd.y;
+
         Ray::new(
-            self.origin,
-            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+            self.origin + offset,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset,
         )
+    }
+}
+
+fn random_in_unit_disk() -> Vec3 {
+    let mut rng = rand::thread_rng();
+    loop {
+        let p = 2.0 * Vec3::new(rng.gen::<f32>(), rng.gen::<f32>(), 0.0) - Vec3::new(1.0, 1.0, 0.0);
+        if p.dot(&p) < 1.0 {
+            return p;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -51,12 +51,19 @@ fn main() {
             Rc::new(Dialectric::new(1.5)),
         )),
     ]);
+
+    let lookfrom = Vec3::new(3.0, 3.0, 2.0);
+    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let dist_to_focus = (lookfrom - lookat).len();
+    let aperture = 2.0f32;
     let camera = Camera::new(
-        Vec3::new(-2.0, 2.0, 1.0),
-        Vec3::new(0.0, 0.0, -1.0),
+        lookfrom,
+        lookat,
         Vec3::new(0.0, 1.0, 0.0),
         20.0,
         nx as f32 / ny as f32,
+        aperture,
+        dist_to_focus,
     );
     let mut rng = rand::thread_rng();
 
